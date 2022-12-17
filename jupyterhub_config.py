1fc72c1fd3ca48263941aaadfb94c19fcd81c24d
camenduru <camenduru@users.noreply.huggingface.co>
2022-12-17 14:04:59 +0000

    Create jupyterhub_config.py

---
+++ jupyterhub_config.py
@@ -0,0 +1,22 @@
+# from jupyterhub.spawner import SimpleLocalProcessSpawner
+
+# c.ServerApp.ip = '0.0.0.0'
+# c.JupyterHub.ip = '0.0.0.0'
+# c.JupyterHub.proxy_api_ip = '0.0.0.0'
+# c.JupyterHub.hub_ip = '0.0.0.0'
+
+# c.Spawner.notebook_dir = '~/notebooks'
+# c.Spawner.args = ['--NotebookApp.default_url=/notebooks/Welcome.ipynb']
+# c.JupyterHub.port = 443
+
+# setting a dummy user admin for now
+c.JupyterHub.authenticator_class = "dummy"
+c.DummyAuthenticator.password = "admin"
+
+# using simplelocalspawner for now
+# c.JupyterHub.spawner_class = SimpleLocalProcessSpawner
+# c.Spawner.cmd = ['jupyter-labhub']
+
+# for creating new users
+c.LocalAuthenticator.add_user_cmd = ['python3','/app/analysis/create-user.py','USERNAME']
+c.LocalAuthenticator.create_system_users = True
